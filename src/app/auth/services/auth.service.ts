c463918fe8ed1072ed586d6d501e271d946ea587
Alvaro <alvaro.ledesmav@gmail.com>
2024-10-16 11:50:53 +0200

    Check token response. Local storage fix for Ang 17

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,10 @@
-import { computed, inject, Injectable, signal } from '@angular/core';
+import { afterNextRender, computed, inject, Injectable, PLATFORM_ID, signal } from '@angular/core';
 import { environment } from '../../../environments/environments';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
-import { AuthStatus, LoginResponse, User } from '../interfaces';
+import { AuthStatus, CheckTokenResponse, LoginResponse, User } from '../interfaces';
+import { platformBrowser } from '@angular/platform-browser';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -15,12 +17,34 @@ export class AuthService {
   private _currentUser = signal< User | null > (null);
   private _authStatus = signal<AuthStatus>( AuthStatus.checking );
 
-
+  platformId =inject(PLATFORM_ID);
 
   public currentUser =  computed( () => this._currentUser() );
   public authStatus = computed( () => this._authStatus() );
 
-  constructor() { }
+  constructor() { 
+    this.checkAuthStatus().subscribe();
+  }
+
+  private setAuthentication( user: User, token: string ):boolean {
+
+    
+    this._currentUser.set( user );
+    this._authStatus.set( AuthStatus.authenticated );
+
+    // afterNextRender(() => {
+      localStorage.setItem('token', token);
+      
+    // })
+
+
+
+    return true;
+  }
+
+  
+
+
 
   login( email: string, password: string): Observable<boolean> {
 
@@ -29,23 +53,44 @@ export class AuthService {
 
     return this.http.post<LoginResponse>( url, body )
       .pipe(
-        tap( ({ user, token }) => {
-          this._currentUser.set( user );
-          this._authStatus.set( AuthStatus.authenticated );
-
-          localStorage.setItem('token', token);
-          console.log({user, token})
-        }),
-        map( () => true ),
-
-
-        catchError( err => {
-          console.log(err)
-
-          return throwError( () => err.error.message );
-        })
-
+        map( ({ user, token }) => this.setAuthentication( user, token )),
+        catchError( err => throwError( () => err.error.message ))
       );
   }
 
+
+  checkAuthStatus(): Observable<boolean>{
+
+    const url = `${ this.baseUrl}/auth/check-token`;
+
+    if(isPlatformBrowser(this.platformId)){
+
+      const token = localStorage.getItem('token');
+
+      if( !token ) {
+        return of( false );
+        
+      } 
+
+      const headers = new HttpHeaders()
+      
+      .set('Authorization', `Bearer ${ token }`);
+
+      return this.http.get<CheckTokenResponse>(url, { headers })
+
+      .pipe(
+        map( ({ user, token }) => this.setAuthentication( user, token )),
+  
+        catchError(() => {
+          
+          this._authStatus.set( AuthStatus.notAuthenticated );
+          return of(false)
+        })
+      )
+    }
+
+    return of(true);
+    
+  }
 }
+
