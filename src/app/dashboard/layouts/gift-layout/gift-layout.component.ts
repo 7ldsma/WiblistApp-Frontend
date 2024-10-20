0c60be825462d0d55552212cd10552e28adaa7ea
Alvaro <alvaro.ledesmav@gmail.com>
2024-10-20 21:07:53 +0200

    gift layout and profile components added

---
+++ src/app/dashboard/layouts/gift-layout/gift-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gift-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './gift-layout.component.html',
+  styleUrl: './gift-layout.component.css'
+})
+export class GiftLayoutComponent {
+
+}
