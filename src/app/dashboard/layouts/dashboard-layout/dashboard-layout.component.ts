bbbd32d24afb11c08ef4b8fc6954c9ad0b0095dc
Alvaro <alvaro.ledesmav@gmail.com>
2024-10-21 11:00:17 +0200

    dashboard component to include gifts-layout

--- src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -2,10 +2,11 @@ import { Component, computed, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidemenuComponent } from '../../../shared/sidemenu/sidemenu.component';
 import { AuthService } from '../../../auth/services/auth.service';
+import GiftLayoutComponent from '../gift-layout/gift-layout.component';
 
 @Component({
   standalone: true,
-  imports: [ RouterOutlet, SidemenuComponent ],
+  imports: [ RouterOutlet, SidemenuComponent, GiftLayoutComponent ],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.css'
 })
