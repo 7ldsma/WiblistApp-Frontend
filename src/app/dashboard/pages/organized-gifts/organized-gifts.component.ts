fc490df3cb59e4ec29dca84eb8631246f7bb1f67
Alvaro <alvaro.ledesmav@gmail.com>
2024-10-28 11:22:11 +0100

    organized gift component update

--- src/app/dashboard/pages/organized-gifts/organized-gifts.component.ts
+++ src/app/dashboard/pages/organized-gifts/organized-gifts.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Gift } from '../../interfaces/gif.interface';
+import { GiftsService } from '../../services/gifts.service';
 
 @Component({
   standalone: true,
@@ -6,6 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './organized-gifts.component.html',
   styleUrl: './organized-gifts.component.css'
 })
-export default class OrganizedGiftsComponent {
+export default class OrganizedGiftsComponent implements OnInit {
+
+  public organizedGifts: Gift[] = [];
+
+  private giftService = inject( GiftsService );
+
+  ngOnInit(): void {
+    this.giftService.getGifts()
+      .subscribe( gifts => this.organizedGifts = gifts );
+  }
 
 }
