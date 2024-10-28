02a7d46c1cc4283d037833a5b99d5268c670ec79
Alvaro <alvaro.ledesmav@gmail.com>
2024-10-28 11:19:17 +0100

    New gift page component added

---
+++ src/app/dashboard/pages/new-gift-page/new-gift-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-gift-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-gift-page.component.html',
+  styleUrl: './new-gift-page.component.css'
+})
+export class NewGiftPageComponent {
+
+}
