b0745557f39afe66fa6502813b6ba9167c943f28
Alvaro <alvaro.ledesmav@gmail.com>
2024-11-06 10:24:30 +0100

    Organized gifts (list) component added

--- src/app/dashboard/pages/organized-gifts/organized-gifts.component.ts
+++ src/app/dashboard/pages/organized-gifts/organized-gifts.component.ts
@@ -1,22 +1,53 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { Gift } from '../../interfaces/gif.interface';
 import { GiftsService } from '../../services/gifts.service';
+import { AuthService } from '../../../auth/services/auth.service';
+import { GiftResponse } from '../../interfaces/gift.response';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './organized-gifts.component.html',
   styleUrl: './organized-gifts.component.css'
 })
 export default class OrganizedGiftsComponent implements OnInit {
 
-  public organizedGifts: Gift[] = [];
+  organizedGifts: Gift[] = [];
+
+  imagePaths: string[] = [];
+
+  private authService = inject( AuthService );
 
   private giftService = inject( GiftsService );
 
+
   ngOnInit(): void {
-    this.giftService.getGifts()
-      .subscribe( gifts => this.organizedGifts = gifts );
+
+    const userId = this.authService.currentUser()!._id
+
+
+    if(userId){
+
+      this.giftService.getGifts(userId)
+        .subscribe( (response: GiftResponse) => {
+          this.organizedGifts = response.organized;
+          this.imagePaths = this.organizedGifts.map(item => 
+            item.alt_image || this.getRandomImage()
+          );
+        });
+
+      }
+      
+  }
+
+  basePath = '../../../../assets/';
+  imageCount = 3;
+
+  getRandomImage(): string {
+
+      const randomIndex = Math.floor(Math.random() * this.imageCount) + 1;
+      return `${this.basePath}${randomIndex}.png`;
   }
 
 }
